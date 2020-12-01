31f2512acdf84aa94237a829ca51411745b4a899
Nishant Sagar <nishantkkr68@gmail.com>
2020-12-01 11:14:06 +0530

    added dashboard page

--- test_web/views.py
+++ test_web/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='login')
+
 def home(request):
     return render(request,'home.html',{})
 
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request,'dashboard.html',{})
